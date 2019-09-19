46f6f742c6ca91e95ed857b28a4a3124f97a7a1f
aiecel <170088@edu.psuti.ru>
2019-09-19 16:00:27 +0400

    Added entry point

---
+++ src/PO73/perepechin/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO73.perepechin.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Igor Perepechin, and I'm not a monkey");
+    }
+}
